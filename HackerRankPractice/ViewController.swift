f8cf9dba68da2edf0cd65c70c90455366fb2c19b
Ronak Shastri <ronak.shastri@scotiabank.com>
2019-07-24 11:51:43 -0400

    Initial Commit

---
+++ HackerRankPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackerRankPractice
+//
+//  Created by Ronak Shastri on 2019-07-24.
+//  Copyright © 2019 Ronak Shastri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
